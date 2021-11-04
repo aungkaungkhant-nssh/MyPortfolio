a0c030b9cdb826450872fb617ee1fbf9f7641455
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-11-04 11:28:53 +0630

    sidebar reuseabel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,11 @@ import { Paper,Tab,Tabs} from '@mui/material'
 import React from 'react'
 import CTab from '../controls/CTab'
 
-const options=[
-    {value:"home",label:"Home"},
-    {value:"about",label:"About"},
-    {value:"skill",label:"Skill"},
-    {value:"project",label:"Project"}
-]
-function Navbar() {
 
+function Navbar({options}) {
     return (
        <Paper>
-           <CTab options={options}/>
+           <CTab options={options} iconsPosition="start"/>
        </Paper>
     )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Paper,Avatar} from '@mui/material'
+import CList from '../controls/CList'
+import { makeStyles } from '@mui/styles';
+const useStyles=makeStyles((theme)=>({
+    root:{
+        display:"flex",flexDirection:"column",alignItems:"center",
+        padding:"20px"
+    },
+    
+}))
+function Sidebar({options}) {
+    const classes=useStyles();
+    return (
+        <Paper>
+            <div className={classes.root}>
+                <Avatar alt="Remy Sharp" src={"https://scontent.xx.fbcdn.net/v/t1.15752-9/s206x206/251497064_3712980925593668_5401179695239504798_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=aee45a&_nc_eui2=AeFabeNtsuHSWiMkiNc-lch3PJSw7Ot4Qz08lLDs63hDPQOw02PSbSPwb79pHIC3qxXQ1l3yESPjP5Tolt9t5Q8j&_nc_ohc=-2WJDRICum0AX-9XQe4&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=96fd34814506362bdfe24aeb0e4cd064&oe=61AADADB"}  sx={{ width:150, height: 150 }} style={{marginBottom:"10px"}}/>
+                <CList options={options} />
+            </div>
+            
+        </Paper>
+    )
+}
+
+export default Sidebar
