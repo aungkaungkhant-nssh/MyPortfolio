93967d94797080212e8ac3fd66c66919d3182760
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-11-12 08:57:59 +0630

    GirlFriend Detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -5,16 +5,15 @@ import {makeStyles} from '@mui/styles'
 // import AboutDetail from './AboutDetail'
 const useStyles=makeStyles((theme)=>({
     detailRoot:{
-       minHeight:"380px",
+       minHeight:"400px",
        marginTop:"10px",
        
     },
-    // '@media screen and (max-width: 600px)': {
-    //     detailRoot: {
-    //       maxHeight:"370px",
-    //       overflowY:"scroll"
-    //     },
-    //   },
+    '@media screen and (max-width: 600px)': {
+        detailRoot: {
+          height:"430px"
+        },
+      },
     
 }))
 function Detail({options,index}) {
